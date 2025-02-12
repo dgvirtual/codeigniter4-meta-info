4eecb6da9b20b23e38869a4c92602a08b28d4e3d
Donatas Glodenis <dg@lapas.info>
2025-02-12 21:54:58 +0200

    Configuration updates

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,33 @@
+<?php
+
+use CodeIgniter\CodingStandard\CodeIgniter4;
+use Nexus\CsConfig\Factory;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->files()
+    ->in([
+        __DIR__ . '/src/',
+        __DIR__ . '/tests/',
+        __DIR__ . '/demo/',
+    ])
+    ->exclude([
+        'build',
+        'src/Components',
+    ])
+    ->append([
+        __FILE__,
+        __DIR__ . '/rector.php',
+    ]);
+
+$overrides = [
+    // 'declare_strict_types' => true,
+    // 'void_return'          => true,
+];
+
+$options = [
+    'finder'    => $finder,
+    'cacheFile' => 'build/.php-cs-fixer.cache',
+];
+
+return Factory::create(new CodeIgniter4(), $overrides, $options)->forProjects();
