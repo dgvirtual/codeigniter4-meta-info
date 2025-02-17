f39e56f44cfe64faac4176be3c67b20d2aba677c
Donatas Glodenis <dg@lapas.info>
2025-02-17 18:21:41 +0200

    add copyright info

--- tests/Feature/DemoFeaturesTest.php
+++ tests/Feature/DemoFeaturesTest.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the codeigniter4-meta-info library.
+ * (c) Donatas Glodenis <dg@lapas.info>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace Tests\Feature;
 
 use CodeIgniter\Test\CIUnitTestCase;
