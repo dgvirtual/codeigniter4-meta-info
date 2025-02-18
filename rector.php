53c64244c68db8739d52c14fad679f461a61ed22
Donatas Glodenis <dg@lapas.info>
2025-02-18 22:27:50 +0200

    Add tools configs and workflows

---
+++ rector.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\CodeQuality\Rector\If_\CombineIfRector;
+use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
+use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector;
+use Rector\CodeQuality\Rector\FuncCall\SimplifyStrposLowerRector;
+use Rector\CodeQuality\Rector\FuncCall\SimplifyRegexPatternRector;
+use Rector\CodeQuality\Rector\Expression\InlineIfToExplicitIfRector;
+use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
+use Rector\CodeQuality\Rector\BooleanAnd\SimplifyEmptyArrayCheckRector;
+use Rector\EarlyReturn\Rector\Return_\PreparedValueToEarlyReturnRector;
+use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
+use Rector\CodeQuality\Rector\Ternary\UnnecessaryTernaryExpressionRector;
+use Rector\CodeQuality\Rector\FuncCall\ChangeArrayPushToArrayAssignRector;
+use Rector\CodeQuality\Rector\Foreach_\UnusedForeachValueToArrayKeysRector;
+use Rector\CodingStyle\Rector\ClassMethod\FuncGetArgsToVariadicParamRector;
+use Rector\Privatization\Rector\Property\PrivatizeFinalClassPropertyRector;
+use Rector\TypeDeclaration\Rector\Property\AddPropertyTypeDeclarationRector;
+use Rector\EarlyReturn\Rector\If_\ChangeIfElseValueAssignToEarlyReturnRector;
+// use Epifrin\RectorCustomRules\RectorRules\ReplaceDoubleQuotesWithSingleRector;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\EarlyReturn\Rector\Foreach_\ChangeNestedForeachIfsToEarlyContinueRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNativeCallRector;
+// use Epifrin\RectorCustomRules\RectorRules\ConvertLocalVariablesNameToCamelCaseRector;
+// use Epifrin\RectorCustomRules\RectorRules\ConvertPrivateMethodsNameToCamelCaseRector;
+// use Rector\CodeQuality\Rector\ClassMethod\ReturnTypeFromStrictScalarReturnExprRector;
+use Rector\CodingStyle\Rector\ClassMethod\MakeInheritedMethodVisibilitySameAsParentRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src/',
+        __DIR__ . '/tests/',
+        __DIR__ . '/demo/',
+    ]);
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+    ]);
+
+    // Include Composer's autoload - required for global execution, remove if running locally
+    $rectorConfig->autoloadPaths([
+        __DIR__ . '/vendor/autoload.php',
+    ]);
+
+    // Auto-import fully qualified class names
+    $rectorConfig->importNames();
+    $rectorConfig->skip([
+        __DIR__ . '/src/Views',
+        __DIR__ . '/demo/Views',
+    ]);
+
+    $rectorConfig->rule(SimplifyUselessVariableRector::class);
+    $rectorConfig->rule(RemoveAlwaysElseRector::class);
+    $rectorConfig->rule(ChangeNestedForeachIfsToEarlyContinueRector::class);
+    $rectorConfig->rule(ChangeIfElseValueAssignToEarlyReturnRector::class);
+    $rectorConfig->rule(SimplifyStrposLowerRector::class);
+    $rectorConfig->rule(CombineIfRector::class);
+    $rectorConfig->rule(SimplifyIfReturnBoolRector::class);
+    $rectorConfig->rule(InlineIfToExplicitIfRector::class);
+    $rectorConfig->rule(PreparedValueToEarlyReturnRector::class);
+    $rectorConfig->rule(ShortenElseIfRector::class);
+    $rectorConfig->rule(SimplifyIfElseToTernaryRector::class);
+    $rectorConfig->rule(UnusedForeachValueToArrayKeysRector::class);
+    $rectorConfig->rule(ChangeArrayPushToArrayAssignRector::class);
+    $rectorConfig->rule(UnnecessaryTernaryExpressionRector::class);
+    $rectorConfig->rule(SimplifyRegexPatternRector::class);
+    $rectorConfig->rule(FuncGetArgsToVariadicParamRector::class);
+    $rectorConfig->rule(MakeInheritedMethodVisibilitySameAsParentRector::class);
+    $rectorConfig->rule(SimplifyEmptyArrayCheckRector::class);
+    $rectorConfig->rule(StringClassNameToClassConstantRector::class);
+    $rectorConfig->rule(PrivatizeFinalClassPropertyRector::class);
+    $rectorConfig->rule(CompleteDynamicPropertiesRector::class);
+    // $rectorConfig->rule(ConvertPrivateMethodsNameToCamelCaseRector::class);
+    // $rectorConfig->rule(ConvertLocalVariablesNameToCamelCaseRector::class);
+    // $rectorConfig->rule(ReplaceDoubleQuotesWithSingleRector::class);
+    $rectorConfig->rule(ReturnTypeFromStrictNativeCallRector::class);
+    $rectorConfig->rule(AddPropertyTypeDeclarationRector::class);
+};
